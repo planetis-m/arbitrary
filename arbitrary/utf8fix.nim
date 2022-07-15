6986e9befaf5fcd44e2adf7cfca048dbd3f7f0c5
Antonis Geralis <b3lievem@yandex.com>
2022-07-15 18:24:15 +0300

    i am bored

--- arbitrary/utf8fix.nim
+++ arbitrary/utf8fix.nim
@@ -6,7 +6,7 @@ proc storeCode(buf: var openArray[char]; e: int, code: uint32, size: int, prefix
   var code = code
   while size > 0:
     dec e
-    buf[e] = char(0x80 or (code and 0x3F))
+    buf[e] = char(0x80 or (code and 0x3f))
     code = code shr 6
     dec size
   dec e
@@ -19,32 +19,32 @@ proc fixCode(buf: var openArray[char], b, e: int, r: var Rand): int =
   var b = b
   var c = uint32(buf[b])
   inc b
-  while b < e and (uint32(buf[b]) and 0xC0) == 0x80:
-    c = c shl 6 + (uint32(buf[b]) and 0x3F)
+  while b < e and (uint32(buf[b]) and 0xc0) == 0x80:
+    c = c shl 6 + (uint32(buf[b]) and 0x3f)
     inc b
   let size = b - start
   case size
   of 1:
-    c = c and 0x7F
+    c = c and 0x7f
     storeCode(buf, b, c, size, 0)
   of 2:
-    c = c and 0x7FF
+    c = c and 0x7ff
     if c < 0x80:
-      c = r.rand(0x80'u32..0x7FF'u32)
-    storeCode(buf, b, c, size, 0xC0)
+      c = r.rand(0x80'u32..0x7ff'u32)
+    storeCode(buf, b, c, size, 0xc0)
   of 3:
-    c = c and 0xFFFF
+    c = c and 0xffff
     # [0xD800, 0xE000) are reserved for UTF-16 surrogate halves.
-    if c < 0x800 or (c >= 0xD800 and c < 0xE000):
-      const halves = 0xE000 - 0xD800
-      c = r.rand(0x800'u32..0xFFFF'u32 - halves)
-      if c >= 0xD800: c = c + halves
-    storeCode(buf, b, c, size, 0xE0)
+    if c < 0x800 or (c >= 0xd800 and c < 0xe000):
+      const halves = 0xe000 - 0xd800
+      c = r.rand(0x800'u32..0xffff'u32 - halves)
+      if c >= 0xd800: c = c + halves
+    storeCode(buf, b, c, size, 0xe0)
   of 4:
-    c = c and 0x1FFFFF
-    if c < 0x10000 or c > 0x10FFFF:
-      c = r.rand(0x10000'u32..0x10FFFF'u32)
-    storeCode(buf, b, c, size, 0xF0)
+    c = c and 0x1fffff
+    if c < 0x10000 or c > 0x10ffff:
+      c = r.rand(0x10000'u32..0x10ffff'u32)
+    storeCode(buf, b, c, size, 0xf0)
   else:
     assert(false, "Unexpected size of UTF-8 sequence")
   return b
@@ -64,17 +64,17 @@ when isMainModule:
   block:
     assert "".isValid
     assert "abc".isValid
-    assert "\xC2\xA2".isValid
-    assert "\xE2\x82\xAC".isValid
-    assert "\xF0\x90\x8D\x88".isValid
-    assert not "\xFF\xFF\xFF\xFF".isValid
-    assert not "\xFF\x8F".isValid
-    assert not "\x3F\xBF".isValid
+    assert "\xc2\xa2".isValid
+    assert "\xe2\x82\xac".isValid
+    assert "\xf0\x90\x8d\x88".isValid
+    assert not "\xff\xff\xff\xff".isValid
+    assert not "\xff\x8f".isValid
+    assert not "\x3f\xbf".isValid
 
   block:
     var str = newString(rand(0..255))
     for run in 1..10000:
-      for i in 0..<str.len: str[i] = rand(0.char..0xFF.char)
+      for i in 0..<str.len: str[i] = rand(low(char)..high(char))
       var fixed = str
       fixUtf8(fixed, randState)
       if str.isValid: assert fixed == str
